6faa4de33f011f17147441210cb657a2f0e98523
Puleeno Nguyen <puleeno@gmail.com>
2020-10-02 18:40:35 +0700

    Create embebable testimonials plugin

---
+++ embeddable-testimonials.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Ramphor Testimonials
+ * Plugin URI: https://github.com/ramphor/embeddable-testimonials
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Version: 1.0.0
+ * Description: Create a testimonials for your site as WordPress plugin or embed to other plugins, themes
+ */
+
+use Ramphor\Testimonials\Testimonials;
+
+define( 'EMBEDDABLE_TESTIMONIALS_PLUGIN_FILE', __FILE__ );
+
+
+if ( ! class_exists( Testimonials::class ) ) {
+	$composerAutoloader = sprintf( '%s/vendor/autoload.php', dirname( EMBEDDABLE_TESTIMONIALS_PLUGIN_FILE ) );
+	if ( file_exists( $composerAutoloader ) ) {
+		require_once $composerAutoloader;
+	}
+}
+
+if ( ! function_exists( 'embeddable_testimonials' ) ) {
+	function embeddable_testimonials() {
+		return Testimonials::getInstance();
+	}
+}
+
+$GLOBALS['embeddable_testimonials'] = embeddable_testimonials();
