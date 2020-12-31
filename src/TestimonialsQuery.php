931c3012dba0ac01c4f4247b84b9fdf85ad84dd3
Puleeno Nguyen <puleeno@gmail.com>
2020-12-31 13:08:05 +0700

    Exclude `none` orderby

--- src/TestimonialsQuery.php
+++ src/TestimonialsQuery.php
@@ -26,7 +26,7 @@ class TestimonialsQuery
             );
         }
 
-        if (!empty($args['orderby'])) {
+        if (!empty($args['orderby']) && $args['orderby'] !== 'none') {
             $ret['orderby'] = $args['orderby'];
             if (isset($args['order']) && in_array(strtolower($args['order']), array('asc', 'desc'))) {
                 $ret['order'] = $args['order'];
