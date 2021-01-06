e5f3dacf5bfc20cb2ab5d2f9b2322d55d5b48654
Puleeno Nguyen <puleeno@gmail.com>
2021-01-06 12:26:29 +0700

    Fix splidejs path

--- src/Testimonials.php
+++ src/Testimonials.php
@@ -95,7 +95,7 @@ final class Testimonials
     {
         global $wp_scripts, $wp_styles;
         if (!isset($wp_scripts->registered['splide'])) {
-            wp_register_script('splide', $this->asset_url('vendor/splide/splide.min.js'), array(), '2.4.21', true);
+            wp_register_script('splide', $this->asset_url('vendor/splide/js/splide.min.js'), array(), '2.4.21', true);
         }
         if (!isset($wp_styles->registered['splide'])) {
             wp_register_style('splide', $this->asset_url('vendor/splide/css/splide-core.min.css'), array(), '2.4.21');
