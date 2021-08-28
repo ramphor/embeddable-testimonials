e75d0c45de39964afb60807bc3462545b23aa67f
Puleeno Nguyen <puleeno@gmail.com>
2021-08-28 15:02:32 +0700

    Support lightbox play video for testimonials

--- src/Display.php
+++ src/Display.php
@@ -1,6 +1,10 @@
 <?php
 namespace Ramphor\Testimonials;
 
+use Jankx\PostFormats\Abstracts\Format;
+use Jankx\PostFormats\PostFormats;
+use Ramphor\Testimonials\Testimonials;
+
 class Display
 {
     protected $testimonials;
@@ -14,12 +18,30 @@ class Display
     public function display()
     {
         $this->templateEngine = $this->testimonials->getTemplateEngine();
-
-        add_action('jankx_testimonial_layout_before_loop_item', array($this, 'createVideoOverlay'));
+        add_action(
+            'jankx_testimonial_layout_before_loop_post_thumbnail',
+            array($this, 'createVideoOverlay'),
+            10,
+            2
+        );
     }
 
-    public function createVideoOverlay($post)
+    public function createVideoOverlay($post, $data_index)
     {
+        $format = Format::getFormat($post);
+        $feature = PostFormats::getFeature($format);
+        if (!$feature) {
+            return;
+        }
+        $testimonial = Testimonials::getInstance();
+        $feature->setTemplateEngine(
+            $testimonial->getTemplateEngine()
+        );
+        switch ($format) {
+            case 'video':
+                return $feature->makeVideoOverlay($post, $data_index);
+        }
+
         if ($post->format() !== 'video') {
             return;
         }
--- templates/post-layout/large-item.php
+++ templates/post-layout/large-item.php
@@ -1,5 +1,5 @@
 <div <?php post_class(array('loop-item', 'post-large-image')); ?>>
-    <?php do_action('jankx_testimonial_layout_before_loop_item', $post); ?>
+    <?php do_action('jankx_testimonial_layout_before_loop_item', $post, $data_index); ?>
     <div class="post-thumbnail">
         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
             <?php jankx_the_post_thumbnail('medium_large'); ?>
@@ -12,5 +12,5 @@
 
         <div class="description"><?php the_excerpt(); ?></div>
     </div>
-    <?php do_action('jankx_testimonial_layout_after_loop_item', $post); ?>
+    <?php do_action('jankx_testimonial_layout_after_loop_item', $post, $data_index); ?>
 </div>
