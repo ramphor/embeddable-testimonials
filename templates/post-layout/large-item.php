41e8215b269a9fbec23a4bbe7d7434783dd8dbdc
Puleeno Nguyen <puleeno@gmail.com>
2021-11-22 18:03:19 +0700

    Change item tags

--- templates/post-layout/large-item.php
+++ templates/post-layout/large-item.php
@@ -6,9 +6,9 @@
         </a>
     </div>
     <div class="post-infos">
-        <h2 class="post-title">
+        <h3 class="post-title">
             <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-        </h2>
+        </h3>
 
         <div class="description"><?php the_excerpt(); ?></div>
     </div>
